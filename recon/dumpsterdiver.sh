a38da45752037ca8afc66371fa2e5d6b56c220bc
oblivia simplex <oblivia@paranoici.org>
2018-03-19 12:49:18 -0300

    nice banner

--- recon/dumpsterdiver.sh
+++ recon/dumpsterdiver.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+script=$0
 bin=$1
 db=$2
 useragent=$3
@@ -19,7 +20,7 @@ PINK=$'\e[01;31m'
 BLACK=$'\e[00;30m'
 BLUE=$'\e[01;34m'
 DARKBLUE=$'\e[00;34m'
-WHITE=$'\e[01;38m'
+WHITE=$'\e[01;37m'
 RESET=$'\e[0m'
 YELLOW=$'\e[01;33m'
 MAGENTA=$'\e[01;35m'
@@ -27,11 +28,96 @@ PURPLE=$'\e[00;35m'
 
 MUTEX=0
 
-[ -n "$bin" ] || bin=ghostbin
-loot=$bin-loot
-[ -n "$useragent" ] || useragent="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
-[ -d "$loot" ] || mkdir $loot
-[ -n "$db" ] || db=visited.db
+UP=$'\033[1A'
+DOWN=$'\033[1B'
+
+BANNER=${GREEN}'
+ __ \                             |              __ \ _)                 
+ |   | |   | __ `__ \  __ \   __| __|  _ \  __|  |   | |\ \   / _ \  __| 
+ |   | |   | |   |   | |   |\__ \ |    __/ |     |   | | \ \ /  __/ |    
+____/ \__,_|_|  _|  _| .__/ ____/\__|\___|_|    ____/ _|  \_/ \___|_|    
+                      _|  '${DARKGREEN}'                                               
+                      __________________
+                      \                 \
+                        \                 \
+                          \                 \
+                            \                 \
+           /-------------------------------------
+         //---------------//                  / |
+       //               //                  / __|
+     //               //                  / /  ||
+   //               //                  / /    ||
+ //_______________//   '${YELLOW}'o o'${DARKGREEN}'            / /      ||      ___/-\___
+------------------------------------/   ------- |     |---------|
+| '${GREEN}'BRUTE FORCE '${DARKGREEN}'|         |'${GREEN}'FIND WEIRD'${DARKGREEN}' |           |      | | | | |
+|  '${GREEN}'PASTEBINS'${DARKGREEN}'  |         |   '${GREEN}'SHIT'${DARKGREEN}'    |           |      | | | | |
+|--------------         ------------|           |      | | | | |
+|                                   |           |      | | | | |
+-------------------------------------------------      |_______|
+
+'
+function greet ()
+{
+
+  default_bin=ghostbin
+  default_ua="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+  default_db="visited.db"
+  [ -n "$bin" ] || bin="$default_bin"
+  loot=$bin-loot
+  [ -n "$useragent" ] || useragent="$default_ua"
+  [ -d "$loot" ] || mkdir $loot
+  [ -n "$db" ] || db="$default_db"
+
+  echo "${DARKGREEN}"
+  echo "${BANNER}"
+  echo "${GREEN}Usage: $script [pastebin name] [sqlite3 db] [user agent]"
+  echo "${DARKGREEN}Using: pastebin:   $bin"
+  echo             "       database:   $db"
+  echo             "       user-agent: \"$useragent\""
+  echo "${GREEN}HIT <ENTER> TO CONTINUE...${RESET}"
+  read
+  echo -e "\n"
+}
+
+function freemem ()
+{
+  free -m | awk '{print $4}' | head -n 2 | tail -n 1
+}
+
+function freemem_echo ()
+{
+  fm=$(freemem)
+  if (( $fm < 128 )); then
+    echo "${PINK}[${fm}M free]${RESET}"
+  else
+    echo "${DARKGREY}[${fm}M free]${RESET}"
+  fi
+}
+
+function lootcount_echo ()
+{
+  echo "${DARKGREY}[$(ls $loot | wc -l | awk '{print $1}') looted]${RESET}"
+}
+
+function httpcode_echo ()
+{
+  code=$1
+  color=$DARKGREY
+  case "$code" in 
+    200)
+      colour=$CYAN
+      ;;
+    403)
+      colour=$PINK
+      ;;
+  esac
+  echo "${DARKGREY}[${colour}${code}${DARKGREY}]${RESET}"
+}
+
+function timestamp_echo ()
+{
+  echo "${DARKGREY}[$(date +"%F %T")]${RESET}"
+}
 
 function mecho ()
 {
@@ -119,6 +205,23 @@ function domain () {
   echo $url | cut -d "/" -f 3
 }
 
+function synopsis () {
+  file="$1"
+  url="$2"
+  clear
+  (
+    echo "${WHITE}--- BEGIN SYNOPSIS ---"
+    echo -n ${GREEN} && head -n 16 $file
+    rem=$(( $(wc -l $file | awk '{print $1}') - 16 ))
+    if (( $rem > 0 )); then
+      (( $rem > 16 )) && rem=16
+      echo -n ${DARKGREEN} && tail -n $rem ${file}
+    fi
+    echo -e "\n${CYAN}| $url\n| $file${RESET}"
+    echo -e "${WHITE}--- END SYNOPSIS ---${RESET}\n\n"
+  ) | mcat
+}
+
 function pii(){
     file=$1
     hits=0
@@ -165,9 +268,9 @@ function action(){
     if (( $APPEND_DOMAIN_TO_DATA )); then
       echo $(domain $url) >> $tmp
     fi
-    mecho -en "\r${RESET} [$(ls $loot | wc -l | awk '{print $1}')] ====${WHITE} $url ${RESET}==== [${response}] "
+    mecho -en "${UP}${UP}\r${RESET}$(timestamp_echo)$(freemem_echo)$(lootcount_echo)$(httpcode_echo $response)\n=====${WHITE} $url ${RESET}=====\n"
     if [ "$response" = "200" ]; then
-      echo
+      echo -e "${DOWN}\n"
       #curl -s -A "${useragent}" "${url}" > $tmp
       md5=$(md5sum $tmp | cut -d ' ' -f 1)
       file=${loot}/${md5}
@@ -185,14 +288,9 @@ function action(){
         mecho -e "\n${CYAN}${url} ---> ${file}${RESET}"
         mv $tmp $file
         #rm ${tmp}.pii
-        (echo -n ${GREEN} && head -n 16 $file) | mcat
-        rem=$(( $(wc -l $file | awk '{print $1}') - 16 ))
-        if (( $rem > 0 )); then
-            (( $rem > 16 )) && rem=16
-            (echo -n ${DARKGREEN} && tail -n $rem ${file}) | mcat
-        fi
+        synopsis $file $url
       else
-          mecho -e "\n${YELLOW}[*] fetched ${url}, but data redundant"
+          mecho -e "\n${YELLOW}[*] fetched ${url}, but data redundant\n"
       fi
     else
         sqlite_execute $db "insert into urls(url, response, pii) values('${url}', '${response}', 0);"
@@ -209,6 +307,11 @@ function throttle(){
     joblist=($(jobs -p))
   done
 }
+
+#######################################
+# Main 
+######################################
+greet
 
 if [ "${joblimit}" = "1" ]; then
     while :; do
